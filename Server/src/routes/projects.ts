5b59d9e33d9d1ada1d9a88db9690332648f57c92
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-14 19:37:54 -0300

    Added multer uploader to post route

--- Server/src/routes/projects.ts
+++ Server/src/routes/projects.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import uploads from "./uploads";
 import { getProjects, addProject } from "../controllers/projects";
 
 const router = Router();
 
 router.get('/getAllProjects', getProjects);
-router.post('/addNewProject', addProject);
+router.post('/addNewProject', uploads.single('coverImage'), addProject);
 
 export default router
