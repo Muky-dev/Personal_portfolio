65b7b9ade8a29299714ae9fdfd97fff3511cf591
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-14 18:32:40 -0300

    initial config

---
+++ Server/src/app.ts
@@ -0,0 +1,32 @@
+import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app: Express = express();
+
+const PORT: string | number = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
+app.use(express.json());
+
+const uri: string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6smfd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', error => console.log(error));
+db.once('open', () => {
+    console.log("Database connected");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on localhost:${PORT}`);
+});
