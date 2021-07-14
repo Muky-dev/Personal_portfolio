0d3fe9d67b48dcf6ffa96a8e8770d2a897ed41e7
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-14 19:38:30 -0300

    Response added to POST function

--- Server/src/controllers/projects.ts
+++ Server/src/controllers/projects.ts
@@ -4,7 +4,7 @@ import Project from '../models/Project';
 
 const getProjects = async (req: Request, res: Response): Promise<void> => {
     try {
-        const projects = await Project.find();
+        const projects: IProject[] = await Project.find();
         res.status(200).json({ projects });
     } catch (error) {
         throw Error(error);
@@ -14,12 +14,16 @@ const getProjects = async (req: Request, res: Response): Promise<void> => {
 const addProject = async (req: Request, res: Response): Promise<void> => {
     try {
         const body = req.body
+        const image = req.file
+
         const project: IProject = new Project ({
             name: body.name,
             url: body.projecturl,
-            image_url: body.image,
+            image_url: image?.path,
             dev_status: body.status
         });
+
+        res.status(201).json({project});
     } catch(error) {
         throw Error(error)
     }
