6c52c97c02899cae88750c8adc97201ab8689428
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-14 18:45:14 -0300

    Project Model

---
+++ Server/src/models/Project.ts
@@ -0,0 +1,25 @@
+import { model, Schema } from 'mongoose';
+
+const projectSchema: Schema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        },
+        dev_status: {
+            type: Boolean,
+            required: true
+        },
+        image_url: {
+            type: String,
+            required: true
+        },
+    },
+    { timestamps: true}
+);
+
+export default model("Project", projectSchema);
