81ad15476eb4d96be42ca75c58d18b2d20dae55d
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-14 20:03:38 -0300

    Delete project route added

--- Server/src/routes/projects.ts
+++ Server/src/routes/projects.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import uploads from "./uploads";
-import { getProjects, addProject } from "../controllers/projects";
+import { getProjects, addProject, deleteProject } from "../controllers/projects";
 
 const router = Router();
 
 router.get('/getAllProjects', getProjects);
 router.post('/addNewProject', uploads.single('coverImage'), addProject);
+router.delete('/deleteProject/:id', deleteProject);
 
 export default router
