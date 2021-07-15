63c4a2611d7fe1667f2c86d8870a841d6b0c5181
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-15 13:58:17 -0300

    Restful pattern and "updateProject" route added

--- Server/src/routes/projects.ts
+++ Server/src/routes/projects.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import uploads from "./uploads";
-import { getProjects, addProject, deleteProject } from "../controllers/projects";
+import { getProjects, addProject, updateProject, deleteProject } from "../controllers/projects";
 
 const router = Router();
 
-router.get('/getAllProjects', getProjects);
-router.post('/addNewProject', uploads.single('coverImage'), addProject);
+router.get('/getProjects', getProjects);
+router.post('/addProject', uploads.single('coverImage'), addProject);
+router.put('/updateProject/:id', updateProject);
 router.delete('/deleteProject/:id', deleteProject);
 
 export default router
