b44ce4683944cc354b2b2c61248287ffa5ef4385
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-16 14:38:40 -0300

    Changed upload module dir

--- Server/src/routes/projects.ts
+++ Server/src/routes/projects.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import uploads from "./uploads";
+import uploads from "../helpers/uploads";
 import { getProjects, addProject, updateProject, deleteProject } from "../controllers/projects";
 
-const router = Router();
+const router: Router = Router();
 
-router.get('/getProjects', getProjects);
-router.post('/addProject', uploads.single('coverImage'), addProject);
-router.put('/updateProject/:id', updateProject);
-router.delete('/deleteProject/:id', deleteProject);
+router.get('/projects', getProjects);
+router.post('/add-project', uploads.single('coverImage'), addProject);
+router.put('/edit-project/:id', updateProject);
+router.delete('/delete-project/:id', deleteProject);
 
 export default router
