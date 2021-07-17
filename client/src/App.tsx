73eb19a0af07db947de6241e323dd5c93143047e
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-17 16:15:11 -0300

    First POC template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import ProjectComponent from './components/project';
+import { getProjects } from './API'
 
 const App: React.FC = () => {
+
+    const [projects, setProjects] = useState<IProject[]>([]);
+
+    useEffect(() => {
+        fetchProjects()
+    });
+
+    const fetchProjects = (): void => {
+        getProjects()
+            .then(({ data: { projects } }: IProject[] | any) => setProjects(projects))
+            .catch((err: Error) => console.log(err));
+    }
+
     return (
-        <h1>Testing</h1>
-    )
+        <main className='App'>
+            <div>
+                <h1>Projects</h1>
+                {projects.map((project: IProject) => (
+                    <ProjectComponent
+                        key={project._id}
+                        project={project}
+                    />
+                )
+                )}
+            </div>
+        </main>
+    );
 }
 
 export default App;
