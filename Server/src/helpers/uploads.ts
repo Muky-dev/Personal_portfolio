eaf9613bc56ad10b10f8d654b523be027c379d62
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-24 17:07:15 -0300

    Only image types

--- Server/src/helpers/uploads.ts
+++ Server/src/helpers/uploads.ts
@@ -2,13 +2,26 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '../public/');
+        cb(null, '../public/covers');
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
     }
 });
 
-const uploads = multer({ storage: storage });
+const uploads = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpeg" || file.mimetype == "image/jpg") {
+            cb(null, true)
+        } else {
+            cb(null, false);
+            return cb(new Error('Only .png, .jpg, .jpeg allowed'));
+        }
+    },
+    limits: {
+        files: 1
+    }
+});
 
 export default uploads
