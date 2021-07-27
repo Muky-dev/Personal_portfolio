51ce6a1fcc2a7852455248235d27d2b61aed93e4
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-27 20:44:18 -0300

    Auth middleware

---
+++ Server/src/middlewares/authentication.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+
+
+export const validateToken = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers['x-access-token'];
+
+    if (!token) {
+        res.status(401).json({ auth: false, message: 'No token provided' });
+    }
+
+    if (typeof token === 'string') {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                res.status(500).json({ auth: false, message: 'Failed to authenticate token' });
+            }
+            if (decoded) {
+                req.user.id = decoded.id;
+                next()
+            }
+        });
+    }
+}
