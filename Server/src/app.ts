1b5f1a8979c63d0f0c1df420f02aca57cb2125e4
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-27 19:12:50 -0300

    Minor adjusts

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import projectsRouter from './routes/projects';
+import authRouter from './routes/auth';
 
 const app: Express = express();
 
@@ -10,11 +11,14 @@ const PORT: string | number = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 app.use(express.json());
+
 app.use('/', projectsRouter);
+app.use('/', authRouter);
 
 const uri: string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6smfd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
-mongoose.set("useFindAndModify", false);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 mongoose.connect(uri, {
     useUnifiedTopology: true,
