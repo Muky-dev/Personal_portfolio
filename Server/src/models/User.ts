6c70aa84ea0f8c9c9c042b5d965c78a935dce745
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-27 11:45:10 -0300

    User Schema added

---
+++ Server/src/models/User.ts
@@ -0,0 +1,23 @@
+import { IUser } from '../types/user';
+import { Schema, model } from 'mongoose';
+
+const userSchema: Schema = new Schema(
+    {
+        username: {
+            type: String,
+            lowercase: true,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        isAdmin: {
+            type: Boolean,
+            required: true
+        }
+    }
+);
+
+export default model<IUser>('User', userSchema);
