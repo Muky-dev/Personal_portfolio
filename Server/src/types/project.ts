ac7c06cf5fdc1e4fa15dde4540963027dc8e4257
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-28 20:20:26 -0300

    Added author to project types

--- Server/src/types/project.ts
+++ Server/src/types/project.ts
@@ -1,8 +1,9 @@
 import { Document } from 'mongoose';
 
 export interface IProject extends Document {
-    name: string,
-    url: string,
-    image_url: string,
+    name: string
+    author: string
+    url: string
+    image_url: string
     dev_status: boolean
 }
