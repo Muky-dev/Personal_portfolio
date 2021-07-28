e105b47e9ce8f22abec19aa71cba6b2610e2f629
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-28 20:56:14 -0300

    logout route added

--- Server/src/routes/auth.ts
+++ Server/src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { register, login } from '../controllers/auth';
+import { register, login, logout } from '../controllers/auth';
 
 const router: Router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/logout', logout);
 
 export default router
