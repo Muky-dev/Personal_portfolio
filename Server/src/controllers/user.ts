ffeb138a387bc98111755d457ee8ab4021dbe0ad
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-29 22:17:29 -0300

    Added controller to recive the token & change pass

--- Server/src/controllers/user.ts
+++ Server/src/controllers/user.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
-import { IUser } from '../types/user';
+import { IDecoded, IUser } from '../types/user';
 import User from '../models/User';
+import bcrypt from 'bcrypt';
+import { verify } from 'jsonwebtoken';
 
 const getUser = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -16,11 +18,34 @@ const getUser = async (req: Request, res: Response): Promise<void> => {
         });
 
     } catch (error) {
-        res.status(500).json({
+        res.status(400).json({
             message: 'Cannot find user',
             error: error
         });
     }
 }
 
-export { getUser }
+const changeForgetedPass = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const {
+            params: { token },
+            body: { newPassword }
+        } = req
+
+        if (!token) {
+            res.status(401).json({ message: 'Please provide a valid token' });
+        }
+
+        const decoded = verify(token, process.env.JWT_FORGOT_SECRET);
+
+        const hashedPass: string = await bcrypt.hash(newPassword, 10);
+
+        await User.findByIdAndUpdate((decoded as IDecoded).id, { password: hashedPass });
+
+        res.status(200).json({ message: 'Successfully changed password' });
+    } catch (error) {
+        res.status(400).json(error);
+    }
+}
+
+export { getUser, changeForgetedPass }
