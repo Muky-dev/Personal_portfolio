04bb43b18974e0337c9b494631d7e3925f356c48
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-29 22:20:14 -0300

    Reset password route added

--- Server/src/routes/user.ts
+++ Server/src/routes/user.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getUser } from '../controllers/user';
+import { getUser, changeForgetedPass } from '../controllers/user';
 
 const router: Router = Router();
 
 router.get('/:username', getUser);
+router.post('/resetPass/:token', changeForgetedPass);
 
 export default router
